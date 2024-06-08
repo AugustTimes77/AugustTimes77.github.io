eb4a86cf14e8676f31160e22bd3a482a24284216
AugustTimes77@gmail.com <AugustTimes77@gmail.com>
2024-06-08 09:21:58 -0400

    added content to website pages

--- js/loadPage.js
+++ js/loadPage.js
@@ -1,29 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function loadContent(filePath) {
-      fetch(filePath)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-          return response.text();
-        })
-        .then(html => {
-          document.getElementById('main-content').innerHTML = html;
-        })
-        .catch(error => {
-          console.error('Error loading the HTML file:', error);
-        });
-    }
-  
-    // Add event listeners to header links
-    document.getElementById('header').addEventListener('click', function(event) {
-      if (event.target.tagName === 'A') {
-        event.preventDefault();
-        const contentUrl = event.target.getAttribute('data-content-url');
-        if (contentUrl) {``
-          loadContent(contentUrl);
+  function loadContent(filePath) {
+    fetch(filePath)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
+        return response.text();
+      })
+      .then(html => {
+        document.getElementById('main-content').innerHTML = html;
+      })
+      .catch(error => {
+        console.error('Error loading the HTML file:', error);
+      });
+  }
+
+  // Function to load home.html when the page loads
+  function loadHomeContent() {
+    loadContent('content/home.html');
+  }
+
+  // Load home.html when the page loads
+  loadHomeContent();
+
+  // Add event listeners to header links
+  document.getElementById('header').addEventListener('click', function(event) {
+    if (event.target.tagName === 'A') {
+      event.preventDefault();
+      const contentUrl = event.target.getAttribute('data-content-url');
+      if (contentUrl) {
+        loadContent(`content/${contentUrl}`); // Assuming content files are in the "content" folder
       }
-    });
+    }
   });
-  
+});
